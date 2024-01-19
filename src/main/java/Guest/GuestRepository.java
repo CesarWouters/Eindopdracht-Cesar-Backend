1218fcd9431bd32e5171d5680f13a5cd469c8d30
Cesar Wouters <chp.wouters@gmail.com>
2024-01-19 13:15:40 +0100

    eersteCommit

---
+++ src/main/java/Guest/GuestRepository.java
@@ -0,0 +1,4 @@
+package Guest;
+
+public interface GuestRepository {
+}
---
+++ src/test/java/com/example/eindopdracht/EindOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindOpdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
